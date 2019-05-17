1d63e47d90917de9b4ea91349cf16d2f2d709e68
Megan Harrington <mewalsh629@gmail.com>
2019-05-16 21:43:47 -0400

    assignment13-13 completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -7,42 +8,47 @@ function onReady() {
     if (!newToDoText.value) { return; }
     toDos.push({
      title: newToDoText.value,
-     complete: false
+     complete: false,
+     id: id
    });
+   id++
    newToDoText.value = '';
 
    renderTheUI();
 
   }
 
-  function renderTheUI() {
-       const toDoList = document.getElementById('toDoList');
+function renderTheUI() {
+   const toDoList = document.getElementById('toDoList');
 
-       toDoList.textContent = '';
+   toDoList.textContent = '';
 
-       toDos.forEach(function(toDo) {
-         const newLi = document.createElement('li');
-         const checkbox = document.createElement('input');
-         checkbox.type = "checkbox";
+   toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
+      checkbox.type = "checkbox";
 
-         newLi.textContent = toDo.title;
-
-         toDoList.appendChild(newLi);
-         newLi.appendChild(checkbox);
-
+      newLi.textContent = toDo.title;
+      deleteButton.textContent = "Delete";
+      deleteButton.setAttribute("class","mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-color-text--yellow");
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      deleteButton.addEventListener('click', function(event){
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      })
    });
- }
-
-   addToDoForm.addEventListener('submit', event => {
-     event.preventDefault();
-     createNewToDo();
-   });
-  renderTheUI();
-
 }
 
-
-
+addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
+});
+}
 
 window.onload = function(){
     onReady();
